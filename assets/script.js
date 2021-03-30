0d2cfa7f26973e4363c2b9d459e16f71038510b1
Seth Torek <seth.torek@gmail.com>
2021-03-29 22:14:48 -0400

    created function to fetch data

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,6 @@
 // DOM VARIABLES --------------------------------------------------
-
-
+var searchBtn = $("#search-btn")
+var searchTermEl = $("#search-term")
 
 
 
@@ -19,7 +19,21 @@
 
 // EVENT LISTENERS ------------------------------------------------
 
+searchBtn.on("click", function() {
+    event.preventDefault();
+    var searchTerm = searchTermEl.val();
+    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + searchTerm + "&appid=bf2c896f347aeadf48d0bd9ae7d3ba10"
 
+    
+
+    fetch(queryURL).then(function(response){
+        return response.json();
+    }).then(function(data){
+        console.log(data);
+    })
+
+
+});
 
 
 
